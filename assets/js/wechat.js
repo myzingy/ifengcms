f6f76bd16a62825d81f8c3db57ed9f3445ffbed5
git <thinkpad@idea-PC>
2015-03-04 12:43:14 +0800

    share

--- assets/js/wechat.js
+++ assets/js/wechat.js
@@ -26,17 +26,17 @@
 				dataType:'jsonp',
 				success:function(wechat_conf){
 					wx.config({
-					    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+					    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 					    appId: wechat_conf.jsapi_sign.appid,
 					    timestamp: wechat_conf.jsapi_sign.timestamp,
 					    nonceStr: wechat_conf.jsapi_sign.noncestr,
 					    signature: wechat_conf.jsapi_sign.signature,
-					    jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+					    jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 					});
 					wx.onMenuShareTimeline(shareConfig);
-					wx.onMenuShareAppMessage(shareConfig);
-					wx.onMenuShareQQ(shareConfig);
-					wx.onMenuShareWeibo(shareConfig);
+					//wx.onMenuShareAppMessage(shareConfig);
+					//wx.onMenuShareQQ(shareConfig);
+					//wx.onMenuShareWeibo(shareConfig);
 				}
 			});
 		}
