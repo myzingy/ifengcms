00604d9ec8f0d6d9ad3c732b9eab7bfb645ff0ae
git <thinkpad@idea-PC>
2015-04-07 17:12:37 +0800

    yidong

---
+++ modules/reply_yidong/controllers/reply_duduxy.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class reply_yidong extends Public_Controller
+{
+	/**
+	 * Constructor
+	 */
+	function reply_yidong(){
+		parent::Public_Controller();
+		// Load the Auth_form_processing class
+		$this->load->library('reply_lib');
+	}
+	function api(){
+		$query=$_SERVER['QUERY_STRING'];
+		parse_str($query,$_GET);
+		$this->site=$this->reply_lib->init();
+		$this->reply_lib->apply();
+	}
+}
---
+++ modules/reply_yidong/views/list.php
@@ -0,0 +1,55 @@
+<h2><?php print $header?></h2>
+
+<div class="buttons">                
+	<a href="<?php print  site_url($wechat_name.'/admin/'.$wechat_name.'/keyword')?>">
+    <?php print  $this->bep_assets->icon('add');?>
+    	添加微信关键字
+    </a>
+</div><br/><br/>
+
+<?php print form_open($wechat_name.'/admin/'.$wechat_name.'/delete')?>
+<table class="data_grid" cellspacing="0">
+    <thead>
+        <tr>
+            <th width=5%><?php print $this->lang->line('general_id')?></th>
+            <th>关键字</th>
+            <th>回复标题</th>
+            <th>回复描述</th>
+            <th>状态</th>
+            <th>修改</th>
+            <th width=10%><?php print form_checkbox('all','select',FALSE)?><?php print $this->lang->line('general_delete')?></th>        
+        </tr>
+    </thead>
+    <tfoot>
+        <tr>
+            <td colspan=6><?php print $pagination;?></td>
+            <td><?php print form_submit('delete','删除','onClick="return confirm(\'确认要删除吗？\');"')?></td>
+        </tr>
+    </tfoot>
+    <tbody>
+        <?php foreach($members->result_array() as $row):
+            // Check if this user account belongs to the person logged in
+            // if so don't allow them to delete it, also if it belongs to the main
+            // admin user don't allow them to delete it
+            $delete  = form_checkbox('select[]',$row['id'],FALSE);  
+			
+			$active =  ($row['status']==0?'tick':'cross');   
+        ?>
+        <tr>
+            <td><?php print $row['id']?></td>
+            <td><?php print $row['title']?></td>
+            <td><?php print $row['subject']?></td>
+            <td width="50%"><?php print $row['content']?></td>
+            <td><?php print $this->bep_assets->icon($active);?></td>
+            <td class="middle"><a href="<?php print site_url($wechat_name.'/admin/'.$wechat_name.'/keyword/'.$row['id'])?>"><?php print $this->bep_assets->icon('pencil');?></a></td>
+            <td><?php print $delete?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php print form_close()?>
+<script type="text/javascript">
+	pageinit=function(){
+		
+	};
+</script>
